5cb0a53b2f419c6e86cffe2fec90df46df2fa043
divyang9431 <pateldivyang9431@gmail.com>
2021-08-20 21:17:22 +0530

    Update header.php

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -37,7 +37,8 @@
     
     <!-- header and footer bar go here-->
     <div class="header header-fixed header-auto-show header-logo-app">
-        <a href="/" class="header-title"><?php the_custom_logo(); ?></a>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="header-title"><?php bloginfo( 'name' ); ?></a>
+        <!-- <a href="/" class="header-title"><?php the_custom_logo(); ?></a> -->
         <a href="#" data-menu="menu-main" class="header-icon header-icon-1"><i class="fas fa-bars"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-dark"><i class="fas fa-sun"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-light"><i class="fas fa-moon"></i></a>
