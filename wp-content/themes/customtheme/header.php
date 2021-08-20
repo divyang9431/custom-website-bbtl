006bbb7751a668b512ea241172eb35b34a41003d
divyang9431 <pateldivyang9431@gmail.com>
2021-08-20 21:18:10 +0530

    Update header.php

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -42,7 +42,7 @@
         <a href="#" data-menu="menu-main" class="header-icon header-icon-1"><i class="fas fa-bars"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-dark"><i class="fas fa-sun"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-2 show-on-theme-light"><i class="fas fa-moon"></i></a>
-        <a href="#" data-menu="menu-highlights" class="header-icon header-icon-3"><i class="fas fa-brush"></i></a>
+        <!-- <a href="#" data-menu="menu-highlights" class="header-icon header-icon-3"><i class="fas fa-brush"></i></a> -->
     </div>
     <div id="footer-bar" class="footer-bar-5">
         <a href="index-components.html"><i data-feather="heart" data-feather-line="1" data-feather-size="21" data-feather-color="red2-dark" data-feather-bg="red2-fade-light"></i><span>Features</span></a>
