7952ebb24f117f8806cb9d01868a7c5a5966557a
divyang9431 <pateldivyang9431@gmail.com>
2021-08-20 19:27:09 +0530

    Update header.php

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -32,72 +32,55 @@
 <?php wp_body_open(); ?>
 
 <div class="statusbar-overlay"></div>
-
 <div class="panel-overlay"></div>
-
 <div class="panel panel-left panel-reveal">
-    <div class="view view-subnav">
-        <div class="pages">
-            <div data-page="panel-leftmenu" class="page pagepanel">
-                <div class="page-content">
-                    <nav class="main_nav_underline">
-                        <!-- Main Menu -->
-                        <?php wp_nav_menu( array( 'theme_location' =>
-                        'menu-1', ) ); ?>
-                    </nav>
-                </div>
+   <div class="view view-subnav">
+      <div class="pages">
+         <div data-page="panel-leftmenu" class="page pagepanel">
+            <div class="page-content">
+               <nav class="main_nav_underline">
+                  <!-- Main Menu -->
+                  <?php wp_nav_menu( array( 'theme_location' => 'menu-1', ) ); ?>
+               </nav>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </div>
-
 <div class="panel panel-right panel-reveal">
-    <div class="user_login_info">
-        <div class="user_thumb">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/page_photo.jpg" alt="" title="" />
-            <div class="user_details">
-                <!-- User Profile image -->
-                <p>
-                    Welcome, <span><?php global $current_user; wp_get_current_user() ; echo $current_user->user_login; ?></span>
-                </p>
-            </div>
-            <div class="user_avatar">
-                <?php $user = wp_get_current_user(); if ( $user ) : ?>
-                <img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" />
-                <?php endif; ?>
-            </div>
-        </div>
-        <nav class="user-nav">
-            <!-- Profile Menu -->
-            <?php wp_nav_menu( array( 'theme_location' =>
-            'menu-2', ) ); ?>
-        </nav>
-    </div>
+   <div class="user_login_info">
+      <div class="user_thumb">
+         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/page_photo.jpg" alt="" title="" />
+         <div class="user_details">
+            <!-- User Profile image -->
+            <p>Welcome, <span><?php global $current_user; wp_get_current_user() ; echo $current_user->user_login; ?></span></p>
+         </div>
+         <div class="user_avatar">
+            <?php $user = wp_get_current_user(); if ( $user ) : ?> <img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" /> <?php endif; ?>
+         </div>
+      </div>
+      <nav class="user-nav">
+         <!-- Profile Menu -->
+         <?php wp_nav_menu( array( 'theme_location' => 'menu-2', ) ); ?>
+      </nav>
+   </div>
 </div>
-
 <div class="views">
-    <div class="view view-main">
-        <div class="pages">
-            <div data-page="index" class="page homepage">
-                <div class="page-content">
-                    <div class="navbarpages">
-                        <div class="navbar_left">
-                            <div class="logo_text">
-                                <!-- Main Logo -->
-                                <?php the_custom_logo(); ?>
-                            </div>
-                        </div>
-
-                        <div class="navbar_right navbar_right_menu">
-                            <a href="#" data-panel="left" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/menu.png" alt="" title="" /></a>
-                        </div>
-                        <div class="navbar_right">
-                            <a href="#" data-panel="right" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/user.png" alt="" title="" /></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<div class="view view-main">
+<div class="pages">
+<div data-page="index" class="page homepage">
+<div class="page-content">
+<div class="navbarpages">
+   <div class="navbar_left">
+      <div class="logo_text">
+         <!-- Logo -->
+         <?php the_custom_logo(); ?>
+      </div>
+   </div>
+   <div class="navbar_right navbar_right_menu">
+      <a href="#" data-panel="left" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/menu.png" alt="" title="" /></a>
+   </div>
+   <div class="navbar_right">
+      <a href="#" data-panel="right" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/user.png" alt="" title="" /></a>
+   </div>
 </div>
