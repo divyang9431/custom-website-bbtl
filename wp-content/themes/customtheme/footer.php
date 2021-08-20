7b15e6574115682b2c2fb642273f5836ddae0db7
divyang9431 <pateldivyang9431@gmail.com>
2021-08-20 22:30:47 +0530

    Update footer.php

--- wp-content/themes/customtheme/footer.php
+++ wp-content/themes/customtheme/footer.php
@@ -17,18 +17,62 @@
     </div>    
     <!-- end of page content-->
     
-    
+    <!-- menu-share.html -->
     <div id="menu-share" 
          class="menu menu-box-bottom menu-box-detached rounded-m" 
-         data-menu-load="menu-share.html"
          data-menu-height="420" 
          data-menu-effect="menu-over">
+		 <h1 class="text-center font-700 font-26 mt-3 pt-2">Share Azures</h1>
+<p class="boxed-text-xl under-heading">
+    Share our page with the world, increase <br>
+    your page exposure with the world.
+</p>
+<div class="divider divider-margins"></div>
+
+<div class="row text-center mr-4 ml-4 mb-0">
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToFacebook icon icon-l bg-facebook rounded-s shadow-l"><i class="fab fa-facebook-f font-22"></i><br></a>
+        <p class="font-11 opacity-70">Facebook</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToTwitter icon icon-l bg-twitter rounded-s shadow-l"><i class="fab fa-twitter font-22"></i><br></a>
+        <p class="font-11 opacity-70">Twitter</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToLinkedIn icon icon-l bg-linkedin rounded-s shadow-l"><i class="fab fa-linkedin-in font-22"></i><br></a>
+        <p class="font-11 opacity-70">LinkedIn</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToMail icon icon-l bg-mail rounded-s shadow-l"><i class="fa fa-envelope font-22"></i><br></a>
+        <p class="font-11 opacity-70">Email</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToWhatsApp icon icon-l bg-whatsapp rounded-s shadow-l"><i class="fab fa-whatsapp font-22"></i><br></a>
+        <p class="font-11 opacity-70">WhatsApp</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToCopyLink icon icon-l bg-blue2-dark rounded-s shadow-l"><i class="fa fa-link font-22"></i><br></a>
+        <p class="font-11 opacity-70">Copy Link</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="shareToPinterest icon icon-l bg-pinterest rounded-s shadow-l"><i class="fab fa-pinterest-p font-22"></i><br></a>
+        <p class="font-11 opacity-70">Pinterest</p>
+    </div>
+    <div class="col-3 mb-n2">
+        <a href="#" class="close-menu icon icon-l bg-red2-dark rounded-s shadow-l"><i class="fa fa-times font-22"></i><br></a>
+        <p class="font-11 opacity-70">Close</p>
+    </div>
+</div>
+
+<div class="divider divider-margins mt-n1 mb-3"></div>
+<p class="text-center font-10 mb-0">Copyright <span class="copyright-year"></span> - Enabled. All rights reserved.</p>
+
+
     </div>    
     
 	<!-- menu-colors.html -->
     <div id="menu-highlights" 
          class="menu menu-box-bottom menu-box-detached rounded-m" 
-         data-menu-load=""
          data-menu-height="510" 
          data-menu-effect="menu-over"> 
 		 <div class="card header-card shape-rounded h-40">
@@ -67,10 +111,10 @@
 		 
     </div>
     
+	<!-- menu-main -->
     <div id="menu-main"
          class="menu menu-box-right menu-box-detached rounded-m"
          data-menu-width="260"
-		 data-menu-load=""
          data-menu-active="nav-welcome"
          data-menu-effect="menu-over"> 
 		 <div class="menu-header">
