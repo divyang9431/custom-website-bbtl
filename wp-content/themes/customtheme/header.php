3ee4f5b9d203eb4ec0c22a2bd17af742617f0aa4
divyang9431 <pateldivyang9431@gmail.com>
2021-08-20 19:17:11 +0530

    Update header.php

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -31,143 +31,76 @@
 <body id="mobile_wrap" <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<div class="statusbar-overlay"></div>
+<div id="page" class="site">
+ <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'customtheme' ); ?></a>
 
-<div class="panel-overlay"></div>
+ <div class="statusbar-overlay"></div>
 
-<div class="panel panel-left panel-reveal">
-<div class="view view-subnav">
-<div class="pages">
-	<div data-page="panel-leftmenu" class="page pagepanel">	
-				 <div class="page-content">
-		<nav class="main_nav_underline">
-		<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-				)
-			);
-			?>
+ <div class="panel-overlay"></div>
 
-		<!-- <ul>
-		<li><a href="index.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/home.png" alt="" title="" /><span>Home</span></a></li>
-		<li><a href="about.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/mobile.png" alt="" title="" /><span>About</span></a></li>
-		<li><a href="features.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/features.png" alt="" title="" /><span>Features</span></a></li>
-		
-		<li><a href="#" data-popup=".popup-login" class="open-popup close-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/lock.png" alt="" title="" /><span>Login</span></a></li>
-		<li><a href="team.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/users.png" alt="" title="" /><span>Team</span></a></li>
-		<li><a href="blog.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/blog.png" alt="" title="" /><span>Blog</span></a></li>		
-
-		<li><a href="photos.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/photos.png" alt="" title="" /><span>Photos</span></a></li>
-		<li><a href="videos.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/video.png" alt="" title="" /><span>Videos</span></a></li>
-		<li><a href="music.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/music.png" alt="" title="" /><span>Music</span></a></li>
-		
-		<li><a href="shop.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/shop.png" alt="" title="" /><span>Shop</span></a></li>
-		<li class="subnav"><a href="categories.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/categories.png" alt="" title="" /><span>Sub level menu</span></a></li>
-		<li><a href="cart.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/cart.png" alt="" title="" /><span>Cart</span></a></li>
-		
-		<li><a href="tables.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/tables.png" alt="" title="" /><span>Tables</span></a></li>
-		<li><a href="chat.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/message.png" alt="" title="" /><span>Chat messages</span></a></li>
-		<li><a href="form.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/form.png" alt="" title="" /><span>Custom Form</span></a></li>
-		
-		
-		<li><a href="tel:012345678" class="close-panel external" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/phone.png" alt="" title="" /><span>Call now!</span></a></li>
-		<li><a href="contact.html" class="close-panel" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/contact.png" alt="" title="" /><span>Contact</span></a></li>
-		</ul> -->
-		
-		</nav>
-				 </div>
-	</div>
+ <div class="panel panel-left panel-reveal">
+  <div class="view view-subnav">
+   <div class="pages">
+    <div data-page="panel-leftmenu" class="page pagepanel">
+     <div class="page-content">
+      <nav class="main_nav_underline">
+       <!-- Main Menu -->
+       <?php wp_nav_menu( array( 'theme_location' =>
+       'menu-1', ) ); ?>
+      </nav>
+     </div>
+    </div>
+   </div>
   </div>
-</div>  
-</div>
+ </div>
 
-<div class="panel panel-right panel-reveal">
+ <div class="panel panel-right panel-reveal">
   <div class="user_login_info">
-  
-			<div class="user_thumb">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/page_photo.jpg" alt="" title="" />
-			  <div class="user_details">
-			   <p>Welcome, <span><?php global $current_user; wp_get_current_user() ; echo $current_user->user_login; ?></span></p>
-			  </div>  
-			  <div class="user_avatar">
-			  <?php $user = wp_get_current_user(); if ( $user ) : ?> <img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" /> <?php endif; ?>	  
-			  <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/avatar.jpg" alt="" title="" /> -->
-			</div>        
-			</div>
-			
-			  <nav class="user-nav">
-			  <?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-2',
-				)
-			);
-			?>
-				<!-- <ul>
-				  <li><a href="features.html" class="close-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/settings.png" alt="" title="" /><span>Account Settings</span></a></li>
-				  <li><a href="features.html" class="close-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/briefcase.png" alt="" title="" /><span>My Account</span></a></li>
-				  <li><a href="features.html" class="close-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/message.png" alt="" title="" /><span>Messages</span><strong>12</strong></a></li>
-				  <li><a href="features.html" class="close-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/love.png" alt="" title="" /><span>Favorites</span><strong>5</strong></a></li>
-				  <li><a href="index.html" class="close-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/lock.png" alt="" title="" /><span>Logout</span></a></li>
-				</ul> -->
-			  </nav>
+   <div class="user_thumb">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/page_photo.jpg" alt="" title="" />
+    <div class="user_details">
+     <!-- User Profile image -->
+     <p>
+      Welcome, <span><?php global $current_user; wp_get_current_user() ; echo $current_user->user_login; ?></span>
+     </p>
+    </div>
+    <div class="user_avatar">
+     <?php $user = wp_get_current_user(); if ( $user ) : ?>
+     <img src="<?php echo esc_url( get_avatar_url( $user->ID ) ); ?>" />
+     <?php endif; ?>
+    </div>
+   </div>
+   <nav class="user-nav">
+    <!-- Profile Menu -->
+    <?php wp_nav_menu( array( 'theme_location' =>
+    'menu-2', ) ); ?>
+   </nav>
   </div>
+ </div>
+
+ <div class="views">
+  <div class="view view-main">
+   <div class="pages">
+    <div data-page="index" class="page homepage">
+     <div class="page-content">
+      <div class="navbarpages">
+       <div class="navbar_left">
+        <div class="logo_text">
+		 <!-- Main Logo -->
+		 <?php the_custom_logo(); ?>
+        </div>
+       </div>
+
+       <div class="navbar_right navbar_right_menu">
+        <a href="#" data-panel="left" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/menu.png" alt="" title="" /></a>
+       </div>
+       <div class="navbar_right">
+        <a href="#" data-panel="right" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/user.png" alt="" title="" /></a>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
 </div>
-
-<div class="views">
-	<div class="view view-main">
-		<div class="pages">
-			<div data-page="index" class="page homepage">
-				<div class="page-content">
-					<div class="navbarpages">
-						<div class="navbar_left">
-							<div class="logo_text">
-							<?php
-							the_custom_logo();
-							?>
-							</div>
-						</div>
-
-						<div class="navbar_right navbar_right_menu">
-							<a href="#" data-panel="left" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/menu.png" alt="" title="" /></a>
-						</div>
-						<div class="navbar_right">
-							<a href="#" data-panel="right" class="open-panel"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/user.png" alt="" title="" /></a>
-						</div>
-						<!-- <div class="navbar_right">
-							<a href="cart.html" data-view=".view-main"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/white/cart.png" alt="" title="" /><span>3</span></a>
-						</div> -->
-					</div>
-				
-			
-		
-
-
-
-
-
-<!-- <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'customtheme' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			
-		</div> -->
-		<!-- .site-branding -->
-
-		<!-- <nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'customtheme' ); ?></button>
-			 <?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		
-		</nav>
-		 -->
-		<!-- #site-navigation -->
-	<!-- </header>#masthead -->
